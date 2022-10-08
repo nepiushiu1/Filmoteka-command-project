cfd203faca823e27058af0540e308e1949aadf58
Anastasiia <guba.anastaysha@gmail.com>
2022-10-09 00:32:38 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
   homeCardsContainer: document.querySelector('.cards__list--home'),
+  libraryCardsContainer: document.querySelector('.cards__list--library'),
   // YOUR ELEMENT IN DOM
 };
