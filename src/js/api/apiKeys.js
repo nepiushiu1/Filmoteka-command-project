28c72d80c2b4085a2a53232867e1df36c7ed4c20
Anastasiia <guba.anastaysha@gmail.com>
2022-10-08 14:12:45 +0300

    deleted

--- src/js/api/apiKeys.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = 'e09f06c48afcb3ebfd8a25b0b6965d1e';
-
-export default API_KEY;
--- src/js/api/baseUrls.js
+++
@@ -1,7 +0,0 @@
-const BASE_TRENDING_MOVIES_URL = 'https://api.themoviedb.org/3/';
-
-const BASE_GENRE_URL = 'https://api.themoviedb.org/3/genre/movie/list?';
-
-const BASE_POSTER_URL = 'https://image.tmdb.org/t/p/w500/';
-
-export { BASE_TRENDING_MOVIES_URL, BASE_POSTER_URL, BASE_GENRE_URL };
