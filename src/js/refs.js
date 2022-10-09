95885b5886536c5e16088a6ef4693e7e788e43cf
Anastasiia <guba.anastaysha@gmail.com>
2022-10-10 01:30:00 +0300

    updated files

--- src/js/refs.js
+++ src/js/refs.js
@@ -10,13 +10,15 @@ export const refs = {
   btnAddToWatched: document.querySelector('.modal_watched'),
   btnAddToQueue: document.querySelector('.modal_queue'),
 
+  closeModalBtn: document.querySelector('.modal-close-button'),
+
   // search
   DEBOUNCE_DELAY: 300,
   formSearch: document.querySelector('.search-form'),
   filmsSearchList: document.querySelector('.search-form__list'),
 
   //modal-card
-  modalCardContainer: document.querySelector('.modal_card'),
+  modalCardContainer: document.querySelector('.movie__container'),
 
   inputSearch: document.querySelector('.search-form__input'),
 };
