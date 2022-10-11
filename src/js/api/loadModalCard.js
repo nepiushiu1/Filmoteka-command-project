8e6aa2b6292436bccb5b9da51dbf2e7a97e56c3f
MarynaYa <yamarina30@gmail.com>
2022-10-11 14:21:36 +0300

    fix-commit

--- src/js/api/loadModalCard.js
+++ src/js/api/loadModalCard.js
@@ -43,11 +43,20 @@ function onModalCardBackdropClick(e) {
 }
 
 function makingModalCardMarkup(obj) {
-  const markup = `<div class="movie__container--left-side">
-                <img class="movie__image" src="https://www.themoviedb.org/t/p/w500${
-                  obj.poster_path
+  const markup = `<div class="movie__container--left-side">              
+                  <img class="movie__image" src="https://www.themoviedb.org/t/p/w500${
+                  obj.poster_path || `https://raw.githubusercontent.com/marvall/filmoteka/main/src/images/no-poster.png`
                 }"
                     alt="${obj.title || obj.name}" />
+
+                    <button type="button" class="movie__btn-trailer">
+                        <svg width='100' height='60' class='youtube-icon' viewBox='0 -77 512.00213 512'>
+                            <path
+                                d='m501.453125 56.09375c-5.902344-21.933594-23.195313-39.222656-45.125-45.128906-40.066406-10.964844-200.332031-10.964844-200.332031-10.964844s-160.261719 0-200.328125 10.546875c-21.507813 5.902344-39.222657 23.617187-45.125 45.546875-10.542969 40.0625-10.542969 123.148438-10.542969 123.148438s0 83.503906 10.542969 123.148437c5.90625 21.929687 23.195312 39.222656 45.128906 45.128906 40.484375 10.964844 200.328125 10.964844 200.328125 10.964844s160.261719 0 200.328125-10.546875c21.933594-5.902344 39.222656-23.195312 45.128906-45.125 10.542969-40.066406 10.542969-123.148438 10.542969-123.148438s.421875-83.507812-10.546875-123.570312zm0 0'
+                                fill='#f00'></path>
+                            <path d='m204.96875 256 133.269531-76.757812-133.269531-76.757813zm0 0' fill='#fff'></path>
+                        </svg>                      
+                    </button>                
             </div>
             <div class="movie__container--rigth-side">
                 <h2 class="movie__title">${obj.title || obj.name}</h2>
@@ -87,28 +96,26 @@ function makingModalCardMarkup(obj) {
                 </table>
                 <h2 class="movie__about-title">About</h2>
                 <p class="movie__about-text">
-                    ${obj.overview}
+                    ${obj.overview || 'No text'}
                 </p>
                 <div class="movie__btn-container">
-                    <button type="button" class="movie__btn btn btn-trailer">
-                        <svg width='20' height='20' class='youtube-icon' viewBox='0 -77 512.00213 512'>
-                            <path
-                                d='m501.453125 56.09375c-5.902344-21.933594-23.195313-39.222656-45.125-45.128906-40.066406-10.964844-200.332031-10.964844-200.332031-10.964844s-160.261719 0-200.328125 10.546875c-21.507813 5.902344-39.222657 23.617187-45.125 45.546875-10.542969 40.0625-10.542969 123.148438-10.542969 123.148438s0 83.503906 10.542969 123.148437c5.90625 21.929687 23.195312 39.222656 45.128906 45.128906 40.484375 10.964844 200.328125 10.964844 200.328125 10.964844s160.261719 0 200.328125-10.546875c21.933594-5.902344 39.222656-23.195312 45.128906-45.125 10.542969-40.066406 10.542969-123.148438 10.542969-123.148438s.421875-83.507812-10.546875-123.570312zm0 0'
-                                fill='#f00'></path>
-                            <path d='m204.96875 256 133.269531-76.757812-133.269531-76.757813zm0 0' fill='#fff'></path>
-                        </svg>
-                        Watch trailer
-                    </button>
+                   
                     <button type="submit" id="watched-btn" data-id="${
                       obj.id
-                    }" class="movie__btn btn btn--accent">add
-                        to Watched</button>
+                    }" class="movie__btn btn btn--accent">ADD TO WATCHED
+                        </button>
                     <button type="submit" id="queue-btn" data-id="${
                       obj.id
-                    }" class="movie__btn btn btn-queue">add to
-                        queue</button>
+                    }" class="movie__btn btn btn-queue">ADD TO QUEUE
+                        </button>
                 </div>
             </div>
         </div>`;
   refs.modalCardContainer.insertAdjacentHTML('beforeend', markup);
 }
+
+
+
+
+
+
