1836aaab0222ed381e187589f096cbdd078cd0c7
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-10-12 00:25:12 +0300

    q,w => Фільтрація раніше доданих фільмів

--- src/js/local_storage.js
+++ src/js/local_storage.js
@@ -27,12 +27,12 @@ export function addQueueLocalStorage(obj) {
     arrayFilmsQueue = JSON.parse(q);
   }
 
+  // Фільтрація раніше доданих фільмів
   const currentId = obj.id;
   if (arrayFilmsWatched.find(el => el.id === currentId)) {
     return;
   }
 
-  // Фільтрація раніше доданих фільмів
   arrayFilmsQueue.push(obj);
   localStorage.setItem('queue', JSON.stringify(arrayFilmsQueue));
   return arrayFilmsQueue;
