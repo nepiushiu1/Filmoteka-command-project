9c83617f30bc4bc409422eb0a30527cc046468f4
Anastasiia <guba.anastaysha@gmail.com>
2022-10-15 15:06:40 +0300

    update rendering of the modal card

--- src/js/api/loadModalCardLibrary.js
+++ src/js/api/loadModalCardLibrary.js
@@ -93,19 +93,20 @@ function onModalCardBackdropClick(e) {
 }
 
 function makingModalCardMarkup(obj) {
+  let imagePath = ``;
+  !obj.poster_path
+    ? (imagePath = `https://raw.githubusercontent.com/marvall/filmoteka/main/src/images/no-poster.png`)
+    : (imagePath = `${BASE_POSTER_URL}/${obj.poster_path}`);
   const markup = `<div class="movie__container--left-side">              
-                  <img class="movie__image" src="https://www.themoviedb.org/t/p/w500${
-                    obj.poster_path ||
-                    `https://raw.githubusercontent.com/marvall/filmoteka/main/src/images/no-poster.png`
-                  }"
+                  <img class="movie__image" src="${imagePath}"
                     alt="${obj.title || obj.name}" />
                     <button type="button" class="movie__btn-trailer">
-                        <svg width='100' height='60' class='youtube-icon' viewBox='0 -77 512.00213 512'>
+                        <svg width='120' height='80' class='youtube-icon' viewBox='0 -77 512.00213 512'>
                             <path
                                 d='m501.453125 56.09375c-5.902344-21.933594-23.195313-39.222656-45.125-45.128906-40.066406-10.964844-200.332031-10.964844-200.332031-10.964844s-160.261719 0-200.328125 10.546875c-21.507813 5.902344-39.222657 23.617187-45.125 45.546875-10.542969 40.0625-10.542969 123.148438-10.542969 123.148438s0 83.503906 10.542969 123.148437c5.90625 21.929687 23.195312 39.222656 45.128906 45.128906 40.484375 10.964844 200.328125 10.964844 200.328125 10.964844s160.261719 0 200.328125-10.546875c21.933594-5.902344 39.222656-23.195312 45.128906-45.125 10.542969-40.066406 10.542969-123.148438 10.542969-123.148438s.421875-83.507812-10.546875-123.570312zm0 0'
                                 fill='#f00'></path>
                             <path d='m204.96875 256 133.269531-76.757812-133.269531-76.757813zm0 0' fill='#fff'></path>
-                        </svg>                      
+                        </svg>
                     </button>                
             </div>
             <div class="movie__container--rigth-side">
@@ -113,31 +114,31 @@ function makingModalCardMarkup(obj) {
                 <table class="movie__info">
                     <tbody>
                         <tr class="movie__info-rows">
-                            <td class="movie__info-name">Vote / Votes</td>
+                            <td class="movie__info-name" data-key="votes">Vote / Votes</td>
                             <td class="movie__info-rating">
-                                <span class="movie__info-rating-value movie__info-rating--accent">${
+                                <span class="movie__info-rating-value movie__info-rating--accent">${setReleaseVote(
                                   obj.vote_average
-                                }</span>
+                                )}</span>
                                 <span class="movie__info-rating-slash">/</span>
-                                <span class="movie__info-rating-value">${
+                                <span class="movie__info-rating-value">${setReleaseVote(
                                   obj.vote_count
-                                }</span>
+                                )}</span>
                             </td>
                         </tr>
                         <tr class="movie__info-rows">
-                            <td class="movie__info-name">Popularity</td>
+                            <td class="movie__info-name" data-key="popularity">Popularity</td>
                             <td class="movie__info-numbers">${
                               obj.popularity
                             }</td>
                         </tr>
                         <tr class="movie__info-rows">
-                            <td class="movie__info-name">Original Title</td>
+                            <td class="movie__info-name" data-key="originalTitle">Original Title</td>
                             <td class="movie__info-value">${
                               obj.original_title || obj.original_name
                             }</td>
                         </tr>
                         <tr class="movie__info-rows movie__info-rows--last">
-                            <td class="movie__info-name">Genre</td>
+                            <td class="movie__info-name" data-key="genre">Genre</td>
                             <td class="movie__info-value">${
                               gettingGenresListForModal(obj.genre_ids) ||
                               'Genre not defined'
@@ -145,7 +146,7 @@ function makingModalCardMarkup(obj) {
                         </tr>
                     </tbody>
                 </table>
-                <h2 class="movie__about-title">About</h2>
+                <h2 class="movie__about-title" data-key="about">About</h2>
                 <p class="movie__about-text">
                     ${obj.overview || 'No text'}
                 </p>
